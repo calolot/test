9a23c5f4aa364cb3905e37a8958a4cd91523dba7
Kyle Calolot <calolot.kylegabriel.5@gmail.com>
2025-04-24 17:55:36 +0800

    Changed

--- DjangoSite/Index/Index/urls.py
+++ DjangoSite/Index/Index/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("myapp.myurls"))
+    path("", include("myapp.myurls")),
+    
 ]
--- DjangoSite/Index/myapp/views.py
+++ DjangoSite/Index/myapp/views.py
@@ -1,5 +1,10 @@
+import os
+from django.conf import settings
+from django.http import FileResponse, Http404
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, "Main.html")
+def consultportal(request):
+    return render(request, "ConsultationPortal.html")
